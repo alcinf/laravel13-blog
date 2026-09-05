aa86e14fc289e73878eb0f198191cda6c5a50491
alcinf <alc_inf@yahoo.com.mx>
2026-09-04 22:08:49 -0600

    Class 1.4. Routes managment

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+
+
+/*
+Route::get('/nosotros/', function ($id) {
+    return 'Nosotros';
+});
+
+Route::get('/contacto', function () {
+    return 'Contacto';
+});
+*/
+
+Route::prefix('/company')->group(function () {
+
+    Route::get('/nosotros/', function () {
+        return 'Nosotros';
+    });
+        
+    Route::get('/contacto', function () {
+        return 'Contacto';s
+    });
+
 });
